1ebab867e905cbf46814010d7dbad1f015f72bfe
SM <hcooch2ch3@gmail.com>
2021-07-18 23:23:23 +0900

    Add SettingsViewController

---
+++ BigTimer/SettingsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SettingViewController.swift
+//  BigTimer
+//
+//  Created by 임성민 on 2021/07/18.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+
+}
