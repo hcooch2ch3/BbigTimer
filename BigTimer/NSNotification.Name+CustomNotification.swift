6e0982587a95d814503b6bb8b9927639640d180f
SM <hcooch2ch3@gmail.com>
2021-07-23 18:32:40 +0900

    Add Custom Notification
    - Change Clock State, Time Over Notification

--- BigTimer/NSNotification.Name+CustomNotification.swift
+++ BigTimer/NSNotification.Name+CustomNotification.swift
@@ -11,5 +11,7 @@ extension NSNotification.Name {
     static let addTime = NSNotification.Name(rawValue: "addTime")
     static let changeTime = NSNotification.Name(rawValue: "changeTime")
     static let changeState = NSNotification.Name(rawValue: "changeState")
+    static let timeOver = NSNotification.Name(rawValue: "timeOver")
     static let updateClock = NSNotification.Name(rawValue: "updateClock")
+    static let changeClockState = NSNotification.Name(rawValue: "changeClockState")
 }
